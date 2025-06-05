2fe53feac193bd10d8c11b39efc5a6809e8bdb46
Tan Yawen <2749934612@qq.com>
2025-06-05 17:27:26 +0800

    初始化

---
+++ src/main/java/com/example/snowiddemo/SnowIdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.snowiddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowIdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowIdDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/snowiddemo/SnowIdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snowiddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowIdDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
